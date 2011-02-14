5a4add7e0426fc139f72cc564971eebae7c3cb37
Dmitry S. Kravtsov <idkravitz@gmail.com>
2011-02-14 19:41:36 +1000

    Complete Huffman

--- huffman.hs
+++ huffman.hs
@@ -3,17 +3,34 @@ module Huffman
 , extract_huffman
 ) where
 
+import Control.Monad
+import Control.Applicative
 import ArchiveCommon(Stream, Byte)
 import qualified Data.ByteString.Lazy as L
 import Data.Array.Diff
 import Data.Array.IArray
-import Data.Bits(setBit)
-import Debug.Trace
+import Data.Bits(setBit, testBit)
 import Data.Char(ord)
+import Data.Int(Int64)
+import qualified Data.Binary as Binary
 
 data Tree = Leaf Int Byte | Branch Int Tree Tree deriving(Show)
 data Bit = Zero | One deriving(Eq, Show)
 type Bits = [Bit]
+
+instance Binary.Binary Tree where
+    put (Leaf count byte) = do Binary.put (0 :: Byte)
+                               Binary.put count
+                               Binary.put byte
+
+    put (Branch count t0 t1) = do Binary.put (1 :: Byte)
+                                  Binary.put count
+                                  Binary.put t0
+                                  Binary.put t1
+    get = do t <- Binary.get :: Binary.Get Byte
+             case t of
+                  0 -> liftM2 Leaf Binary.get Binary.get
+                  1 -> liftM3 Branch Binary.get Binary.get Binary.get
 
 getCount (Leaf c _)     = c
 getCount (Branch c _ _) = c
@@ -50,7 +67,22 @@ bitsToStream bs = toByte h `L.cons` bitsToStream t
                       0 (zip bs [7,6..0])
 
 encode :: DiffArray Byte Bits -> Stream -> Stream
-encode cb = bitsToStream .  encoded_bits cb
+encode cb = bitsToStream . encoded_bits cb
+
+streamToBits :: Stream -> Bits
+streamToBits s
+    | L.null s  = []
+    | otherwise = (getBit x) ++ streamToBits xs
+    where (x, xs) = (L.head s, L.tail s)
+          getBit x = foldl (\ a i -> (if testBit x i then One else Zero) : a) [] [0..7]
+
+decode :: Tree -> Stream -> Stream
+decode t s = bitDecode t . streamToBits $ s
+    where bitDecode :: Tree -> Bits -> Stream
+          bitDecode (Branch _ t0 t1) (b:bs)
+              | b == Zero = bitDecode t0 bs
+              | b == One  = bitDecode t1 bs
+          bitDecode (Leaf _ b) bs = b `L.cons` bitDecode t bs
 
 buildTree = build . map (\ (b, c) -> Leaf c b) . filter (\ (b, c) -> c /= 0)
     where
@@ -60,8 +92,9 @@ buildTree = build . map (\ (b, c) -> Leaf c b) . filter (\ (b, c) -> c /= 0)
               (t1, ts1) = getTree ts0
           in build $ Branch (getCount t0 + getCount t1) t0 t1 : ts1
 
-
 compress_huffman s = let t = buildTree . assocs . countBytes $ s
                          cb = createCodebook t
-                     in show t `trace` encode cb s
-extract_huffman  = id
+                     in Binary.encode (t, L.length s, encode cb $ s)
+
+extract_huffman s = let (t, l, bs) = (Binary.decode s) :: (Tree, Int64, Stream)
+                    in L.take l . decode t $ bs
