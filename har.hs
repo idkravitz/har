917899ac19a6662201d3f250fe42ece49ef64b2f
Dmitry S. Kravtsov <idkravitz@gmail.com>
2011-03-15 22:25:08 +1000

    Start work on multifile support

--- har.hs
+++ har.hs
@@ -17,12 +17,31 @@ compress Huffman = compressHuffman
 extract  RLE     = extractRLE
 extract  Huffman = extractHuffman
 
+produceCompression :: String -> [String] -> Method -> IO ()
+produceCompression output inputs method = return()
+
+produceExtraction :: String -> Method -> IO ()
+produceExtraction input method = return ()
+
 main = do
     (opts, nopts) <- catch (getArgs >>= archiverOpts) $ \ err -> do
                         putStrLn $ ioeGetErrorString err
                         exitWith $ ExitFailure 1
-    let in_file  = head nopts
-        out_file = fromMaybe "" . optOutput $ opts 
-        func     = if optCompress opts then compress else extract
+    
+    let outFile = fromMaybe "" . optOutput $ opts
         method   = optMethod opts
-    func method (L.readFile in_file) >>= L.writeFile out_file
+
+-- if func == compress then for each input file we ask for a size on
+-- disk, then we write its name and its size (that were on disk), then
+-- we write a compressed stream
+-- if func == extract then we assert that there is only one input file
+-- we ignore the optOutput, and read in a loop name, size, then we
+-- extract only size-bytes.
+
+    if optCompress opts then produceCompression outFile nopts method
+       else produceExtraction (head nopts) method
+
+--    forM nopts \ inFile -> do
+--        func     = if optCompress opts then compress else extract
+--        method   = optMethod opts
+--    func method (L.readFile in_file) >>= L.writeFile out_file
